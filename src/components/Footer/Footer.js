014bb1ec907abc96799fce58f17993422c574e69
Shnayder Danila <shnd3r@yandex.ru>
2023-11-25 19:39:38 +0700

    feat: Add footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Footer.css';
+
+function Footer() {
+
+  return (
+    <footer className='footer'>
+      <p className='footer__copyright'>© 2002–2023, PROSEPT</p>
+    </footer>
+  )
+};
+
+export default Footer;
