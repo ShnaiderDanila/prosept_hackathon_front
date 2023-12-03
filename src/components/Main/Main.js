20e6f4dbff783bcdde51226d937bcb5056910806
Shnayder Danila <shnd3r@yandex.ru>
2023-12-03 14:17:30 +0700

    fix: Fix markup and styling in Main component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,21 +4,15 @@ import './Main.css';
 
 import Dealer from '../Dealer/Dealer';
 import Manufacturer from '../Manufacturer/Manufacturer';
-import FileForm from '../FileForm/FileForm';
 
 export default function Main(props) {
-  // console.log(props);
   return (
     <main className='main'>
-        <FileForm />
-      <div className="all-products_container">
-        <Manufacturer
+      <Manufacturer
         onComparePosition={props.onComparePosition}
         onNotComparePosition={props.onNotComparePosition}
-        onPostonePosition={props.onPostonePosition}
-        />
-        <Dealer />
-      </div>
+        onPostonePosition={props.onPostonePosition} />
+      <Dealer />
     </main>
   )
 }
