53e1b8172a8c7e63c2d9f3ded5d60b2d16375eb4
Shnayder Danila <shnd3r@yandex.ru>
2023-12-07 17:28:27 +0700

    feat: Add transmission of new props and connect preloader in Dealer component

--- src/components/Dealer/Dealer.js
+++ src/components/Dealer/Dealer.js
@@ -3,15 +3,20 @@ import React from 'react';
 import './Dealer.css';
 
 import DealerProductTable from './DealerProductTable/DealerProductTable';
+import Preloader from '../Preloader/Preloader';
 
 function Dealer(props) {
   return (
     <section className='dealer'>
       <h2 className='dealer__title'>Товары дилеров</h2>
-      <DealerProductTable
-        pendingDealersProducts={props.pendingDealersProducts}
-        getRecomendationToDealerProduct={props.getRecomendationToDealerProduct}
-      />
+      {props.isLoadingDealerProducts
+        ? <Preloader />
+        : <DealerProductTable
+          pendingDealersProducts={props.pendingDealersProducts}
+          getRecomendationToDealerProduct={props.getRecomendationToDealerProduct}
+          dealers={props.dealers}
+        />}
+
     </section>
   )
 };
