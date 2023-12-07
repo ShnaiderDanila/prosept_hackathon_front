3a337a94240be146562eed3f7194259972a71c37
Shnayder Danila <shnd3r@yandex.ru>
2023-12-07 17:56:36 +0700

    feat: Add transmission of new props and connect a preloader and fix css styles in the GeneralAnalyticsTable component

--- src/components/GeneralAnalytics/GeneralAnalyticsTable/GeneralAnalyticsTable.js
+++ src/components/GeneralAnalytics/GeneralAnalyticsTable/GeneralAnalyticsTable.js
@@ -1,17 +1,16 @@
 import { useMemo } from 'react';
-import { useTable, useSortBy, useGlobalFilter, useFilters, usePagination } from 'react-table';
+import { useTable, useSortBy, useFilters, usePagination } from 'react-table';
 
 import './GeneralAnalyticsTable.css';
 
-import GeneralAnalyticsFilter from '../GeneralAnalyticsFilter/GeneralAnalyticsFilter';
 import GeneralAnalyticsPagination from '../GeneralAnalyticsPagination/GeneralAnalyticsPagination';
-import GeneralAnalyticsDropdownMenu from '../GeneralAnalyticsDropdownMenu/GeneralAnalyticsDropdownMenu';
+import GeneralAnalyticsFilter from '../GeneralAnalyticsFilter/GeneralAnalyticsFilter';
+import Preloader from '../../Preloader/Preloader';
 
 const COLUMNS = [
   {
     Header: 'Наименование товара',
     accessor: 'product_name',
-    disableFilters: true,
     Cell: ({ row }) =>
       <a
         className='dealer-table__product-link'
@@ -33,20 +32,19 @@ const COLUMNS = [
   {
     Header: 'Дата записи',
     accessor: 'date',
-    disableFilters: true,
   },
   {
     Header: 'Статус сопоставления',
     accessor: 'status',
-    disableFilters: true,
+    disableSortBy: true,
   },
 ]
 
-function GeneralAnalyticsTable({ dealers, dealersProducts, setSelectedDealer }) {
+function GeneralAnalyticsTable({ dealers, dealersProducts, setSelectedDealer, isLoadingGeneralAnalytics }) {
 
   const defaultColumn = useMemo(() => {
     return {
-      Filter: GeneralAnalyticsDropdownMenu
+      Filter: GeneralAnalyticsFilter
     }
   }, []);
 
@@ -54,7 +52,7 @@ function GeneralAnalyticsTable({ dealers, dealersProducts, setSelectedDealer }) 
     columns: COLUMNS,
     data: dealersProducts,
     defaultColumn: defaultColumn,
-  }, useFilters, useGlobalFilter, useSortBy, usePagination);
+  }, useFilters, useSortBy, usePagination);
 
   const {
     getTableProps,
@@ -71,64 +69,67 @@ function GeneralAnalyticsTable({ dealers, dealersProducts, setSelectedDealer }) 
     setPageSize,
     prepareRow,
     state,
-    setGlobalFilter,
   } = tableInstance;
 
-  const { globalFilter, pageIndex, pageSize } = state;
+  const { pageIndex, pageSize } = state;
 
   return (
     <div className='general-analytics-table__wrapper'>
       <h2 className='general-analytics-table__title'>Отчет по товарам дилеров</h2>
-      <GeneralAnalyticsFilter filterValue={globalFilter} setFilterValue={setGlobalFilter} />
-      <table className='general-analytics-table' {...getTableProps()}>
-        <thead>
-          {headerGroups.map((headerGroup) => (
-            <tr className='general-analytics-table__row' {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column) => (
-                <th className='general-analytics-table__header-cell' {...column.getHeaderProps()}>
-                  <div className='general-analytics-table__container'>
-                    <div {...column.getSortByToggleProps()} className='general-analytics-table__sort-header'>
-                      <div className='general-analytics-table__sort-title'>{column.render('Header')}</div>
-                      <p className='general-analytics-table__sorted-icon'>
-                        {column.isSorted ? (column.isSortedDesc ? '▼' : '▲') : ''}
-                      </p>
-                    </div>
-                    <div>
-                      {column.canFilter ? column.render('Filter', {setSelectedDealer: setSelectedDealer, dealers: dealers}) : null}
-                    </div>
-                  </div>
-                </th>
+      {isLoadingGeneralAnalytics
+        ? <Preloader />
+        : <>
+          <table className='general-analytics-table' {...getTableProps()}>
+            <thead>
+              {headerGroups.map((headerGroup) => (
+                <tr className='general-analytics-table__row' {...headerGroup.getHeaderGroupProps()}>
+                  {headerGroup.headers.map((column) => (
+                    <th className='general-analytics-table__header-cell' {...column.getHeaderProps()}>
+                      <div className='general-analytics-table__container'>
+                        <div {...column.getSortByToggleProps()} className='general-analytics-table__sort-header'>
+                          <div className='general-analytics-table__sort-title'>{column.render('Header')}</div>
+                          <p className='general-analytics-table__sorted-icon'>
+                            {column.isSorted ? (column.isSortedDesc ? '▼' : '▲') : ''}
+                          </p>
+                        </div>
+                        <div>
+                          {column.canFilter ? column.render('Filter', { setSelectedDealer: setSelectedDealer, dealers: dealers }) : null}
+                        </div>
+                      </div>
+                    </th>
+                  ))}
+                </tr>
               ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          {page.map((row) => {
-            prepareRow(row)
-            return (
-              <tr {...row.getRowProps()}>
-                {
-                  row.cells.map((cell) => {
-                    return <td className='general-analytics-table__cell' {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                  })
-                }
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
-      <GeneralAnalyticsPagination
-        pageIndex={pageIndex}
-        pageOptions={pageOptions}
-        gotoPage={gotoPage}
-        pageSize={pageSize}
-        setPageSize={setPageSize}
-        nextPage={nextPage}
-        previousPage={previousPage}
-        canPreviousPage={canPreviousPage}
-        canNextPage={canNextPage}
-        pageCount={pageCount}
-      />
+            </thead>
+            <tbody {...getTableBodyProps()}>
+              {page.map((row) => {
+                prepareRow(row)
+                return (
+                  <tr {...row.getRowProps()}>
+                    {
+                      row.cells.map((cell) => {
+                        return <td className='general-analytics-table__cell' {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                      })
+                    }
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+          <GeneralAnalyticsPagination
+            pageIndex={pageIndex}
+            pageOptions={pageOptions}
+            gotoPage={gotoPage}
+            pageSize={pageSize}
+            setPageSize={setPageSize}
+            nextPage={nextPage}
+            previousPage={previousPage}
+            canPreviousPage={canPreviousPage}
+            canNextPage={canNextPage}
+            pageCount={pageCount}
+          />
+        </>
+      }
     </div>
   )
 };
